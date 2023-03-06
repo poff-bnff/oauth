e57ab670bcd7ee08a8a363c6842283bd99d6b934
Argo Roots <argo@roots.ee>
2023-03-06 11:41:46 +0200

    Return test API request info

--- server/api/hello.js
+++ server/api/hello.js
@@ -1,5 +1,8 @@
-export default defineEventHandler((event) => {
-  return {
-    api: 'works'
-  }
+export default defineEventHandler(async (event) => {
+  const method = getMethod(event)
+  const headers = getRequestHeaders(event)
+  const query = getQuery(event)
+  const body = method === 'POST' ? await readBody(event) : undefined
+
+  return { method, headers, query, body }
 })
