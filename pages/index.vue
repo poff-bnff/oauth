bc28b54f76c8f6b922abf447c7203d82848b42b5
Argo Roots <argo@roots.ee>
2023-03-07 16:15:53 +0200

    Fix back link

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@ function getEventivalUrl () {
 
 <template>
   <main>
-    <a class="back" href="#" @click.prevent="() => history?.back()">
+    <a class="back" href="javascript:history.back()">
       <div class="h-5 w-5 flex">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16"><g fill="none"><path d="M13.5 8.5a.5.5 0 0 0 0-1H3.803l4.031-3.628a.5.5 0 1 0-.668-.744l-5 4.5a.5.5 0 0 0 0 .744l5 4.5a.5.5 0 1 0 .668-.744L3.803 8.5H13.5z" fill="currentColor" /></g></svg>
       </div>
