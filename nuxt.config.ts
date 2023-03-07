275af7ad14ba250dd37fb7b7445edb2ae06f6204
Argo Roots <argo@roots.ee>
2023-03-07 12:40:49 +0200

    Add page meta and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+      title: 'PÖFF | BNFF',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/BNFF-192.png' }
+      ]
+    }
+  },
   css: ['~/assets/css/tailwind.css'],
   modules: [
     ['@nuxtjs/google-fonts', {
