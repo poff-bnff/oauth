1f31da0159bb019fd8e9ebab0d54f10346d307ea
Argo Roots <argo@roots.ee>
2023-03-07 14:19:31 +0200

    Get user token from OAuth.ee

--- server/api/auth/oauth.get.js
+++ server/api/auth/oauth.get.js
@@ -2,8 +2,20 @@ export default defineEventHandler(async (event) => {
   const headers = getRequestHeaders(event)
   const query = getQuery(event)
 
-  const redirectCookie = useCookie(event, 'redirect_uri')
-  const stateCookie = useCookie(event, 'state')
+  const redirectCookie = getCookie(event, 'redirect_uri')
+  const stateCookie = getCookie(event, 'state')
 
-  return { headers, query, redirectCookie: redirectCookie.value, stateCookie: stateCookie.value }
+  const { oauthClientId, oauthClientSecret } = useRuntimeConfig()
+
+  const body = {
+    client_id: oauthClientId,
+    client_secret: oauthClientSecret,
+    code: query.code,
+    grant_type: 'authorization_code',
+    state: stateCookie.value
+  }
+
+  const { data } = useFetch('https://oauth.ee/token', { method: 'POST', body })
+
+  return { headers, query, redirectCookie: redirectCookie.value, stateCookie: stateCookie.value, data }
 })
