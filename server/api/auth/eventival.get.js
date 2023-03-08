e45e89ab7146b882c479a2464333bd84499f8e19
Argo Roots <argo@roots.ee>
2023-03-08 14:12:25 +0200

    Add eventival auth

--- server/api/auth/eventival.get.js
+++ server/api/auth/eventival.get.js
@@ -1,6 +1,45 @@
-export default defineEventHandler(async (event) => {
-  const headers = getRequestHeaders(event)
-  const query = getQuery(event)
+import jwt from 'jsonwebtoken'
 
-  return { headers, query }
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const query = getQuery(event)
+  const redirectUri = getCookie(event, 'redirect_uri') || '/?jwt='
+  const stateCookie = getCookie(event, 'state')
+
+  setCookie(event, 'redirect_uri', null)
+  setCookie(event, 'state', null)
+
+  if (!query.code || !query.state || query.state !== stateCookie) {
+    throw createError({ statusCode: 400, statusMessage: 'Invalid arguments' })
+  }
+
+  const body = {
+    client_id: config.public.eventivalClientId,
+    client_secret: config.eventivalClientSecret,
+    code: query.code,
+    grant_type: 'authorization_code',
+    state: stateCookie
+  }
+
+  try {
+    const token = await $fetch(`${config.public.eventivalUrl}/auth/realms/Eventival/protocol/openid-connect/token`, { method: 'POST', body })
+
+    return { token }
+    // const user = await $fetch(`${config.public.eventivalUrl}/user`, { headers: { Authorization: `Bearer ${token}` } })
+
+    // if (!user.email) throw createError({ statusCode: 500, statusMessage: 'No Eventival e-mail' })
+
+    // const strapiUser = await getStrapiUser(user.email)
+
+    // const jwtData = { ...strapiUser }
+    // delete jwtData.id
+
+    // const jwtToken = jwt.sign(jwtData, config.jwtSecret, { expiresIn: '14d', notBefore: 0, subject: strapiUser.id })
+
+    // return sendRedirect(event, redirectUri + jwtToken, 302)
+  } catch (error) {
+    console.error(error)
+
+    throw createError({ statusCode: 500, statusMessage: 'Eventival error' })
+  }
 })
