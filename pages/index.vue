4e42b46f7b5a026b77d4ea352100f1cb092ba064
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-10-14 15:40:18 +0300

    Revert "redirect to eventival, if from industry"
    This reverts commit f3f7534b0b03ad33b16bd14f0d4ef8538ce786cc.

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,47 @@
 <script setup>
-import crypto from "crypto";
+import crypto from 'crypto'
 
-const {
-  url,
-  eventivalClientId,
-  eventivalUrl,
-  oauthClientId,
-  oauthUrl,
-} = useRuntimeConfig();
-const { locale, t } = useI18n();
-const route = useRoute();
-const router = useRouter();
-const redirectCookie = useCookie("redirect_uri");
-const stateCookie = useCookie("state");
+const { url, eventivalClientId, eventivalUrl, oauthClientId, oauthUrl } = useRuntimeConfig()
+const { locale, t } = useI18n()
+const route = useRoute()
+const router = useRouter()
+const redirectCookie = useCookie('redirect_uri')
+const stateCookie = useCookie('state')
 
-redirectCookie.value = route.query.redirect_uri;
-const referrer = route.query.referrer || document.referrer;
+redirectCookie.value = route.query.redirect_uri
 
-locale.value = route.query.locale || "et";
+locale.value = route.query.locale || 'et'
 
 if (!stateCookie.value) {
-  stateCookie.value = crypto.randomBytes(16).toString("hex");
+  stateCookie.value = crypto.randomBytes(16).toString('hex')
 }
 
-watch(
-  () => locale.value,
-  (value) => {
-    router.replace({ query: { ...route.query, locale: value } });
-  }
-);
+watch(() => locale.value, (value) => {
+  router.replace({ query: { ...route.query, locale: value } })
+})
 
-function getOauthUrl(provider) {
+function getOauthUrl (provider) {
   const query = new URLSearchParams({
     client_id: oauthClientId,
     redirect_uri: `${url}/api/auth/oauth`,
-    response_type: "code",
-    scope: "openid",
-    state: stateCookie.value,
-  }).toString();
+    response_type: 'code',
+    scope: 'openid',
+    state: stateCookie.value
+  }).toString()
 
-  return `${oauthUrl}/auth/${provider}?${query}`;
+  return `${oauthUrl}/auth/${provider}?${query}`
 }
 
-function getEventivalUrl() {
+function getEventivalUrl () {
   const query = new URLSearchParams({
     client_id: eventivalClientId,
     redirect_uri: `${url}/api/auth/eventival`,
-    response_type: "code",
-    scope: "openid",
-    state: stateCookie.value,
-  }).toString();
+    response_type: 'code',
+    scope: 'openid',
+    state: stateCookie.value
+  }).toString()
 
-  return `${eventivalUrl}/auth/realms/Eventival/protocol/openid-connect/auth?${query}`;
-}
-// if referrer domain contains 'industry', then redirect to eventival auth
-if (referrer.includes("industry.")) {
-  window.location.href = getEventivalUrl();
+  return `${eventivalUrl}/auth/realms/Eventival/protocol/openid-connect/auth?${query}`
 }
 </script>
 
@@ -63,24 +49,16 @@ if (referrer.includes("industry.")) {
   <main>
     <div class="w-full flex items-start justify-between gap-4">
       <a class="back" href="javascript:history.back()">
-        <svg
-          class="h-5 w-5 flex"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 16 16"
-        >
+        <svg class="h-5 w-5 flex" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16">
           <g fill="none">
-            <path
-              d="M13.5 8.5a.5.5 0 0 0 0-1H3.803l4.031-3.628a.5.5 0 1 0-.668-.744l-5 4.5a.5.5 0 0 0 0 .744l5 4.5a.5.5 0 1 0 .668-.744L3.803 8.5H13.5z"
-              fill="currentColor"
-            />
+            <path d="M13.5 8.5a.5.5 0 0 0 0-1H3.803l4.031-3.628a.5.5 0 1 0-.668-.744l-5 4.5a.5.5 0 0 0 0 .744l5 4.5a.5.5 0 1 0 .668-.744L3.803 8.5H13.5z" fill="currentColor" />
           </g>
         </svg>
-        {{ t("back") }}
+        {{ t('back') }}
       </a>
 
-      <a v-if="locale !== 'en'" @click.prevent="() => (locale = 'en')">EN</a>
-      <a v-else @click.prevent="() => (locale = 'et')">ET</a>
+      <a v-if="locale !== 'en'" @click.prevent="() => locale = 'en'">EN</a>
+      <a v-else @click.prevent="() => locale = 'et'">ET</a>
     </div>
 
     <div class="w-full flex flex-col sm:flex-row items-start justify-center gap-4">
@@ -88,34 +66,34 @@ if (referrer.includes("industry.")) {
         <a class="auth" :href="getOauthUrl('apple')">Apple</a>
         <a class="auth" :href="getOauthUrl('google')">Google</a>
         <a class="auth" :href="getEventivalUrl()">Eventival</a>
-        <a class="auth" :href="getOauthUrl('e-mail')">{{ t("email") }}</a>
+        <a class="auth" :href="getOauthUrl('e-mail')">{{ t('email') }}</a>
       </div>
       <div class="w-full flex flex-col gap-4">
         <a class="auth" :href="getOauthUrl('smart-id')">Smart-ID</a>
-        <a class="auth" :href="getOauthUrl('mobile-id')">{{ t("mobileid") }}</a>
-        <a class="auth" :href="getOauthUrl('id-card')">{{ t("idcard") }}</a>
+        <a class="auth" :href="getOauthUrl('mobile-id')">{{ t('mobileid') }}</a>
+        <a class="auth" :href="getOauthUrl('id-card')">{{ t('idcard') }}</a>
       </div>
     </div>
 
     <div>
-      <p>{{ t("info") }}</p>
+      <p>{{ t('info') }}</p>
     </div>
   </main>
 </template>
 
 <i18n lang="yaml">
-en:
-  back: Back
-  idcard: ID-card
-  mobileid: Mobile-ID
-  email: E-Mail
-  info: PÖFF (Tallinn Balck Nights Film Festival) user identification service allows you to identify yourself with an e-mail, ID-card, Mobile-ID, Smart-ID or an Apple, Google and Eventival account. E-mail, ID-card, Mobile-ID, Smart-ID, Apple and Google authentication is provided by OAuth.ee.
-et:
-  back: Tagasi
-  idcard: ID-kaart
-  mobileid: Mobiil-ID
-  email: E-post
-  info: PÖFFi kasutajate tuvastamisteenus lubab sul end tuvastada e-posti, ID-kaardi, Mobiil-ID, Smart-ID või Apple'i, Google'i ja Eventivali kontoga. E-posti, ID-kaardi, Mobiil-ID, Smart-ID, Apple'i ja Google'i autentimist vahendab OAuth.ee.
+  en:
+    back: Back
+    idcard: ID-card
+    mobileid: Mobile-ID
+    email: E-Mail
+    info: PÖFF (Tallinn Balck Nights Film Festival) user identification service allows you to identify yourself with an e-mail, ID-card, Mobile-ID, Smart-ID or an Apple, Google and Eventival account. E-mail, ID-card, Mobile-ID, Smart-ID, Apple and Google authentication is provided by OAuth.ee.
+  et:
+    back: Tagasi
+    idcard: ID-kaart
+    mobileid: Mobiil-ID
+    email: E-post
+    info: PÖFFi kasutajate tuvastamisteenus lubab sul end tuvastada e-posti, ID-kaardi, Mobiil-ID, Smart-ID või Apple'i, Google'i ja Eventivali kontoga. E-posti, ID-kaardi, Mobiil-ID, Smart-ID, Apple'i ja Google'i autentimist vahendab OAuth.ee.
 </i18n>
 
 <style scoped>
@@ -123,7 +101,7 @@ main {
   @apply w-full md:w-96;
   @apply mx-auto p-8;
   @apply flex flex-col items-center justify-center gap-16;
-  font-family: "Fira Sans Extra Condensed", sans-serif;
+  font-family: 'Fira Sans Extra Condensed', sans-serif;
 }
 
 p {
@@ -142,7 +120,7 @@ a.back {
 }
 
 a.auth {
-  @apply py-2 px-4;
+  @apply py-2 px-4 ;
   @apply border border-gray-500;
   @apply hover:text-white hover:bg-orange-400 hover:border-orange-400;
   @apply font-normal;
