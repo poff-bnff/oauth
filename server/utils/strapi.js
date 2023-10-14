aa4f409d0d4cc3385f30e512b88f48424d9e7820
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-10-14 20:13:09 +0300

    log less

--- server/utils/strapi.js
+++ server/utils/strapi.js
@@ -34,8 +34,6 @@ export async function authenticateStrapiUser (email) {
 export async function getEventivalBadges(email) {
   if (!email) return []
 
-  console.log (config)
-  return []
   const edition = config.public.eventivalEdition
   const token = config.eventivalApiToken
   const options = {
